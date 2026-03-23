363783ff647a70dc5fee002e2c6084728407c2e5
Alexey Demin <AlexeyDeminA@gmail.com>
2026-03-23 23:20:25 +0300

    feat: set toggleterm window to float

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,1 +1,8 @@
-require("toggleterm").setup { open_mapping = [[<C-\>]] }
+require("toggleterm").setup { 
+  open_mapping = [[<C-\>]],
+  shell = vim.fn.has('win32') == 1 and 'powershell' or 'zsh',
+  direction = "float",
+  float_opts = {
+    border = "rounded"
+  },
+}
