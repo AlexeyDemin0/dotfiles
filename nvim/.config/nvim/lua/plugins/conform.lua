4ede4ad89274dd5af05971cc69f4cb3fd5532c56
Alexey Demin <AlexeyDeminA@gmail.com>
2026-06-15 23:53:59 +0300

    feat(nvim): add prettier formatter for conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,13 +4,29 @@ require("conform").setup({
     h = { "clang-format" },
     cppm = { "clang-format" },
     python = {
-       "ruff_fix",
-       "ruff_format",
-       "ruff_organize_imports",
+      "ruff_fix",
+      "ruff_format",
+      "ruff_organize_imports",
     },
     dart = { "dart_format" },
     rust = { "rustfmt" },
-  }
+    lua = { "stylua" },
+    javascript = { "prettierd", "prettier" },
+    typescript = { "prettierd", "prettier" },
+    css = { "prettierd", "prettier" },
+    scss = { "prettierd", "prettier" },
+    markdown = { "prettierd", "prettier" },
+    html = { "prettierd", "prettier" },
+    json = { "prettierd", "prettier" },
+    yaml = { "prettierd", "prettier" },
+    graphql = { "prettierd", "prettier" },
+    md = { "prettierd", "prettier" },
+  },
+  formatters = {
+    stylua = {
+      args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
+    },
+  },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
